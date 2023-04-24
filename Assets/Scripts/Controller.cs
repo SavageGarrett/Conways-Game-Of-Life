4552d7115574b4166c30af9b3597bb8217836644
SavageGarrett <carder.320@osu.edu>
2023-04-23 22:59:48 -0400

    Simulation Working
    TODO: Scale Up Simulation and add JSON Serialization

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,66 +1,125 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
-{
-    [Header("Scene")]
-    [SerializeField]
-    GameObject camera;
-
-    [SerializeField]
-    int simulationSize = 10;
-
-    [Header("Dead Cells")]
-
-    [SerializeField]
-    GameObject objectParent;
-
-    [SerializeField]
-    GameObject deadPrefab;
-
-    List<List<GameObject>> deadCells;
-
-    [Header("Live Cells")]
-    [SerializeField]
-    GameObject liveParent;
-
-    [SerializeField]
-    GameObject livePrefab;
-
-    List<List<GameObject>> liveCells;
-
-    // Start is called before the first frame update
-    void Start()
+namespace CarderGarrett.Lab6 {
+    public class Controller : MonoBehaviour
     {
-        PrimeCells(deadCells, deadPrefab, 0f);
-        PrimeCells(liveCells, livePrefab, -1.0f);
+        [Header("Scene")]
+        [SerializeField]
+        GameObject camera;
 
-        // Place and rotate the camera
-        camera.transform.position = new Vector3(simulationSize * 0.5f, simulationSize * 0.5f, simulationSize * 0.5f);
-        camera.transform.rotation = Quaternion.Euler(90, 0, 0);
-    }
+        [SerializeField]
+        int simulationSize = 10;
 
-    void PrimeCells(List<List<GameObject>> cells, GameObject prefab, float yPos)
-    {
-        // Initialize the cell list
-        cells = new List<List<GameObject>>();
-        for (int row = 0; row < simulationSize; row++)
+        [Header("Dead Cells")]
+
+        [SerializeField]
+        GameObject deadParent;
+
+        [SerializeField]
+        GameObject deadPrefab;
+
+        List<List<GameObject>> deadCells;
+
+        [Header("Live Cells")]
+        [SerializeField]
+        GameObject liveParent;
+
+        [SerializeField]
+        GameObject livePrefab;
+
+        List<List<GameObject>> liveCells;
+
+        Grid dataRep;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            List<GameObject> columnList = new List<GameObject>();
-            for (int col = 0; col < simulationSize; col++)
-            {
-                GameObject cell = Instantiate(prefab, objectParent.transform);
-                cell.transform.position = new Vector3(row + 0.5f, yPos, col + 0.5f);
-                columnList.Add(cell);
-            }
-            cells.Add(columnList);
-        }
-    }
+            PrimeCells(ref deadCells, deadPrefab, deadParent, 0f);
+            PrimeCells(ref liveCells, livePrefab, liveParent, -1.0f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            dataRep = new Grid(simulationSize);
+
+            // Place and rotate the camera
+            camera.transform.position = new Vector3(simulationSize * 0.5f, simulationSize * 0.5f, simulationSize * 0.5f);
+            camera.transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+
+        void PrimeCells(ref List<List<GameObject>> cells, GameObject prefab, GameObject parent, float yPos)
+        {
+            // Initialize the cell list
+            cells = new List<List<GameObject>>();
+            for (int row = 0; row < simulationSize; row++)
+            {
+                List<GameObject> columnList = new List<GameObject>();
+                for (int col = 0; col < simulationSize; col++)
+                {
+                    GameObject cell = Instantiate(prefab, parent.transform);
+                    cell.transform.position = new Vector3(row + 0.5f, yPos, col + 0.5f);
+                    columnList.Add(cell);
+                }
+                cells.Add(columnList);
+            }
+        }
+
+        float timer = 0f;
+        void Update()
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 1f)
+            {
+                List<List<bool>> currentSimState = dataRep.updateState();
+                PrintGridDebug(currentSimState);
+                UpdateVisualGrid(currentSimState);
+                timer = 0f;
+            }
+        }
+
+        void UpdateVisualGrid(List<List<bool>> currentSimState) {
+            // Loop through each row of the nested list
+            for (int i = 0; i < currentSimState.Count; i++) {
+                // Loop through each element in the current row
+                for (int j = 0; j < currentSimState[i].Count; j++) {
+                    // If the current element is true, hide the live cell plane
+                    if (currentSimState[i][j]) {
+                        deadCells[i][j].SetActive(false);
+                    }
+                    // If the current element is false, show the dead cell plane
+                    else {
+                        deadCells[i][j].SetActive(true);
+                    }
+                }
+            }
+        }
+
+
+        void PrintGridDebug(List<List<bool>> currentSimState) {
+            // Initialize the StringBuilder to concatenate the row strings
+            StringBuilder gridStringBuilder = new StringBuilder();
+            // Loop through each row of the nested list
+            for (int i = 0; i < currentSimState.Count; i++) {
+                // Initialize the row StringBuilder to concatenate the element characters
+                StringBuilder rowStringBuilder = new StringBuilder();
+                // Loop through each element in the current row
+                for (int j = 0; j < currentSimState[i].Count; j++) {
+                    // If the current element is true, add '█' to the row StringBuilder
+                    if (currentSimState[i][j]) {
+                        rowStringBuilder.Append("█");
+                    }
+                    // If the current element is false, add '░' to the row StringBuilder
+                    else {
+                        rowStringBuilder.Append("░");
+                    }
+                }
+                // Append the current row string to the grid StringBuilder
+                gridStringBuilder.AppendLine(rowStringBuilder.ToString());
+            }
+            // Log the entire grid string using Debug.Log
+            Debug.Log(gridStringBuilder.ToString());
+        }
+
     }
 }
--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,18 +1,126 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Grid : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace CarderGarrett.Lab6 {
+    public class Grid
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public List<List<bool>> currentState;
+        private List<List<bool>> nextState;
+        int size;
+
+        public Grid(int size)
+        {
+            this.size = size;
+            currentState = WipeGrid();
+            nextState = WipeGrid();
+
+            // Add some still lifes to the current state
+            currentState[1][1] = true;
+            currentState[1][2] = true;
+            currentState[2][1] = true;
+            currentState[2][2] = true;
+
+            // Add some oscillators to the current state
+            currentState[5][5] = true;
+            currentState[5][6] = true;
+            currentState[5][7] = true;
+        }
+
+        List<List<bool>> WipeGrid()
+        {
+            List<List<bool>> grid = new List<List<bool>>();
+
+            for (int row = 0; row < size; row++)
+            {
+                List<bool> columnList = new List<bool>();
+                for (int col = 0; col < size; col++)
+                {
+                    columnList.Add(false);
+                }
+                grid.Add(columnList);
+            }
+
+            return grid;
+        }
+
+        public List<List<bool>> updateState()
+        {
+            nextState = WipeGrid();
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    int liveNeighbors = getNumberOfNeighbors(row, col);
+
+                    nextState[row][col] = getNextState(currentState[row][col], liveNeighbors);
+                }
+            }
+
+            currentState = nextState;
+
+            return currentState;
+        }
+
+
+        bool getNextState(bool currentState, int numNeighbors)
+        {
+            if (currentState)
+            {
+                // Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+                // Any live cell with more than three live neighbors dies, as if by overpopulation.
+                if (numNeighbors < 2 || numNeighbors > 3)
+                {
+                    return false;
+                }
+                // Any live cell with two or three live neighbors lives on to the next generation.
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+                if (numNeighbors == 3)
+                {
+                    return true;
+                }
+            }
+
+            return currentState;
+        }
+
+        int getNumberOfNeighbors(int row, int col)
+        {
+            int liveNeighbors = 0;
+
+            // Iterate over the neighboring cells
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                for (int j = col - 1; j <= col + 1; j++)
+                {
+                    // Skip the current cell
+                    if (i == row && j == col)
+                    {
+                        continue;
+                    }
+
+                    // Check if the neighboring cell is within the bounds of the grid
+                    if (i >= 0 && i < size && j >= 0 && j < size)
+                    {
+                        // If the neighboring cell is alive, increment the live neighbor count
+                        if (currentState[i][j])
+                        {
+                            liveNeighbors++;
+                        }
+                    }
+                }
+            }
+
+            return liveNeighbors;
+        }
+
     }
 }
